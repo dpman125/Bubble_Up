165db8ca732aa92e55ddc0019e2310e568b5368a
TerminusTech <dsb00015@mix.wvu.edu>
2025-01-23 18:47:42 -0500

    Reworked Cannon Mechanics
    Cannon now sits in corner.  Left/Right controls rotation and Up/Down controls launch power.  Spacebar still launches bubble, which now slows down and gradually floats up.

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class Bubble : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float BubbleSpeed;
+    public Rigidbody2D rb;
+    //public float BubbleSpeed;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        //rb.AddForce(BubbleSpawner.Launch);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, BubbleSpeed * Time.deltaTime, 0f);
+        //transform.Translate(0f, BubbleSpeed * Time.deltaTime, 0f);
         DeleteBubble();
     }
 
--- Assets/Scripts/BubbleSpawner.cs
+++ Assets/Scripts/BubbleSpawner.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class BubbleSpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float Speed;
+    public float AimSpeed;
+    public float PowerSpeed;
+    public float LaunchMod;
     public GameObject Bubble;
     private Vector3 offset;
+    public Vector2 Aim;
+    public Vector2 Launch;
     void Start()
     {
       
@@ -14,21 +18,35 @@ public class BubbleSpawner : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+        //Fire
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            offset = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-            Instantiate(Bubble, offset, transform.rotation);
+            offset = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            var _b = Instantiate(Bubble, offset, transform.rotation);
+            _b.GetComponent<Rigidbody2D>().linearVelocity = Launch;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        
+        //Aim
+        if (Input.GetKey(KeyCode.RightArrow) && Aim.x < 90)
         {
-            transform.Translate(-Speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
+            Aim.x += AimSpeed * Time.deltaTime;
+        }        
+        if (Input.GetKey(KeyCode.LeftArrow) && Aim.x > 0)
         {
-            transform.Translate(Speed * Time.deltaTime, 0f, 0f);
+            Aim.x -= AimSpeed * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.UpArrow) && Aim.y < 1)
+        {
+            Aim.y += PowerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && Aim.y > 0)
+        {
+            Aim.y -= PowerSpeed * Time.deltaTime;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, -Aim.x);
+        Launch = transform.up * Aim.y * LaunchMod;
     }
 
    
